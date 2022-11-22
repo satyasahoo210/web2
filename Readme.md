8bd3e638bd7208409f009e12c270ab3811aed223
Satya Sahoo <satyasahoo210@gmail.com>
2022-11-22 19:37:08 +0530

    day 1 (22 Nov, 2022)

--- Readme.md
+++ Readme.md
@@ -2,3 +2,6 @@
 > This repository contains tutorial material for Web 2.0. (assuming you have a little bit of programming knowledge)
 
 ## Contents
+### Day 1
+- Basic HTML and Introduction to DOM
+- Javascript datatypes and data structures
---
+++ day1/index.js
@@ -0,0 +1,124 @@
+// Traditional -- Hello World
+console.log('Hello world!')
+
+// Datatypes
+
+let num1 = 1;          // number
+let num2 = 2.1;        // number
+let num3 = 123e5;        // number
+
+// Arithmatic operations on numbers
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+// Type checking
+console.log(typeof num1, num1)
+console.log(typeof num2, num2)
+console.log(typeof num3, num3)
+
+// Statically typed language --> C, C++, JAVA, C#
+// Dynamically typed language --> JS, Python
+
+// Dynamically changing datatype
+num1 = 'string'
+console.log(typeof num1)
+
+
+/**
+ * datatypes
+ * =========
+ * number
+ * ------
+ * Integres
+ * Floats
+ * Double
+ * Infinity
+ * NaN
+ * 
+ * string
+ * null
+ * undefined
+ * boolean
+ * 
+ * data structure
+ * ==============
+ * array
+ * js object -> (map ds)
+ * set
+ * 
+ * function
+ */
+
+let string = 'Hello'
+let _null = null
+// let undefine = undefined
+let undefine
+
+let bool = true
+
+// --------------
+let arr  = [1,2,3,1,2,3]
+let set = new Set([1,2,3,1,2,3])
+let obj = {1: 'one', 2: 'two', 3: 'three'}
+
+
+// function add(a,b) {
+
+// }
+
+// let add = function(a,b) {
+//     return a+b;
+// }
+
+
+// let add = (a,b) => a+b // arrow function
+
+
+/**
+// =========================================================
+For Practice
+// =========================================================
+**/
+
+/*
+Pattern 1
+---------
+Input: 5
+
+Output:
+*****
+****
+***
+**
+*
+
+Pattern 2
+---------
+Input: 5
+
+Output:
+*****
+ ****
+  ***
+   **
+    *
+
+Pattern 3
+---------
+Input: 5
+
+Output:
+*****
+****
+***
+**
+*
+**
+***
+****
+*****
+
+
+*/
