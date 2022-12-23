4259600936c68820b3c07296ff6bc4819643da0e
Satya Sahoo <satyasahoo210@gmail.com>
2022-12-23 19:55:35 +0530

    js explained

--- day9/script.js
+++ day9/script.js
@@ -1,19 +1,39 @@
 const todoUl = document.getElementById("todo-list")
-const input = document.getElementById("input")
+
+const input = document.getElementById("input-field")
 const addButton = document.getElementById("add-todo")
-
-function addToTodoList() {
-    let data = input.value
-    
-    let listItem = document.createElement('li')
-    listItem.textContent = data
-
-    todoUl.appendChild(listItem)
-
-    input.value = ''
-}
 
 addButton.addEventListener('click', addToTodoList)
 
+function addToTodoList() {
+    // 1. Get value from `input` box
+    let data = input.value
+    
+    // 2. Create `li` element
+    let listItem = document.createElement('li')
+    // 3. Add textContent
+    listItem.textContent = data
+
+    // 4. Add the `li` element to `ul`
+    todoUl.appendChild(listItem)
+
+    // 5. Clear input value
+    input.value = ''
+}
 
 
+
+/**
+ * document API
+ * ------------
+ * - addEventListener
+ * - append
+ * - appendChild
+ * - createElement
+ * - getElementById
+ * - getElementsByClassName
+ * - getElementsByTagName
+ * - getElementsByName
+ * - querySelecotr
+ * - querySelectorAll
+ */
