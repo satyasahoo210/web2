18d48ab0ffe760f869af15f5a388c66fa0eb7012
Satya Sahoo <satyasahoo210@gmail.com>
2023-01-03 19:57:24 +0530

    added `fetch` functionality

--- day9/script.js
+++ day9/script.js
@@ -3,6 +3,8 @@ const todoUl = document.getElementById("todo-list")
 const input = document.getElementById("input-field")
 const addButton = document.getElementById("add-todo")
 
+const todos = []
+
 addButton.addEventListener('click', addToTodoList)
 
 function addToTodoList() {
@@ -16,10 +18,36 @@ function addToTodoList() {
 
     // 4. Add the `li` element to `ul`
     todoUl.appendChild(listItem)
+    todos.push(data)
 
     // 5. Clear input value
     input.value = ''
 }
+
+function addToTodoListFromArray() {
+    for(let todo of todos) {
+        let listItem = document.createElement('li')
+        listItem.textContent = todo
+
+        todoUl.appendChild(listItem)
+    }
+}
+
+function main() {
+    // Fetch TODOs
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(function(json) {
+            for(let data of json) {
+                todos.push(data.title)
+            }
+        })
+        .then(addToTodoListFromArray)
+}
+
+
+window.onload = main
+
 
 
 
@@ -37,3 +65,9 @@ function addToTodoList() {
  * - querySelecotr
  * - querySelectorAll
  */
+
+
+// Create (INSERT) - POST
+// Read   (SELECT) - GET
+// Update (UPDATE) - PUT / PATCH
+// Delete (DELETE) - DELETE
